a68ee2a1ca007346c49fda484c12142515a855e5
Iván Brian Zamaniego <Ivozama@hotmail.com>
2025-08-11 22:09:52 -0300

    sorteo de amigos

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = ["Ariadna", "Bruno", "Karina", "Carlos"];
-function agregarAmigo() {
-    let input = document.getElementById("amigo");
-    let nombre = input.value.trim();
-
-    if (nombre === "") {
-        alert("Por favor, inserte un nombre.");
-        return;
-    }
-
-    amigos.push(nombre);
-    input.value = "";
-}
-
-function mostrarListaAmigos() {
-    let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
-
-    for (let i = 0; i < amigos.length; i++) {
-        lista.innerHTML += `<li>${amigos[i]}</li>`;
-    }
-}
 
 function agregarAmigo() {
     let input = document.getElementById("amigo");
@@ -36,3 +15,27 @@ function agregarAmigo() {
 
     mostrarListaAmigos();
 }
+
+function mostrarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indice];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
+
+
